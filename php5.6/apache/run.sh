1e53e598701bf5d7e46a5cfcfbaa1b5fef4f5f64
Yousan_O <yousan@l2tp.org>
2016-10-18 23:36:25 +0900

    adding dnsmasq

--- php5.6/apache/run.sh
+++ php5.6/apache/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-if [ ! -f /www/config.secret.inc.php ] ; then
-    cat > /www/config.secret.inc.php <<EOT
+if [ ! -f /var/www/pma.dev/config.secret.inc.php ] ; then
+    cat > /var/www/pma.dev/config.secret.inc.php <<EOT
 <?php
 \$cfg['blowfish_secret'] = '`cat /dev/urandom | tr -dc 'a-zA-Z0-9~!@#$%^&*_()+}{?></";.,[]=-' | fold -w 32 | head -n 1`';
 EOT
@@ -10,13 +10,11 @@ if [ ! -f /config.user.inc.php ] ; then
   touch /config.user.inc.php
 fi
 
-mkdir -p /var/nginx/client_body_temp
-chown nobody:nobody /sessions /var/nginx/client_body_temp
-mkdir -p /var/run/php/
-chown nobody:nobody /var/run/php/
-touch /var/log/php7.0-fpm.log
-chown nobody:nobody /var/log/php7.0-fpm.log
+#mkdir -p /var/nginx/client_body_temp
+#chown nobody:nobody /sessions /var/nginx/client_body_temp
+#mkdir -p /var/run/php/
+#chown nobody:nobody /var/run/php/
 
-if [ "$1" = 'phpmyadmin' ]; then
-    exec supervisord --nodaemon --configuration="/etc/supervisord.conf" --loglevel=info
-fi
+#if [ "$1" = 'phpmyadmin' ]; then
+#    exec supervisord --nodaemon --configuration="/etc/supervisord.conf" --loglevel=info
+#fi
